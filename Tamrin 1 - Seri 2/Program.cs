3a782527622a22f413901b062f91ffb8d6bc2d12
SINA <SINA@SINA>
2024-11-03 23:12:46 +0330

    Add project files.

---
+++ Tamrin 1 - Seri 2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter first number (n): ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number (m): ");
+        int m = Convert.ToInt32(Console.ReadLine());
+
+        // اطمینان از اینکه n کمتر از m باشد
+        if (n > m)
+        {
+            int temp = n;
+            n = m;
+            m = temp;
+        }
+
+        Console.WriteLine($"Prime numbers between {n} and {m}:");
+        for (int i = n; i <= m; i++)
+        {
+            if (IsPrime(i))
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+    static bool IsPrime(int number)
+    {
+        if (number <= 1) return false;
+        if (number == 2) return true;
+        if (number % 2 == 0) return false;
+
+        for (int i = 3; i <= Math.Sqrt(number); i += 2)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
